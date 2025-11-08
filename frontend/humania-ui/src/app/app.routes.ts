2d2063463a052bfb86f999e3cefa6a34ea60dc33
Antonio2772 <atn.randr@gmail.com>
2025-11-08 13:04:52 +0300

    fix: adjust old configurations for the new pages

--- frontend/humania-ui/src/app/app.routes.ts
+++ frontend/humania-ui/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { EmployeListComponent } from './pages/employes/employe-list/employe-list.component';
+import { authGuard } from './core/guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] },
+  { path: 'employes', component: EmployeListComponent, canActivate: [authGuard] }
+];
