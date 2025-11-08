ecab57abfc42218471090615c20a81723bc34cef
Antonio2772 <atn.randr@gmail.com>
2025-11-08 14:46:28 +0300

    chore: adjust the routes for the functionnalities poste

--- frontend/humania-ui/src/app/app.routes.ts
+++ frontend/humania-ui/src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { LoginComponent } from './pages/login/login.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { EmployeListComponent } from './pages/employes/employe-list/employe-list.component';
 import { EmployeDetailComponent } from './pages/employes/employe-detail/employe-detail.component';
+import { PosteListComponent } from './pages/postes/poste-list/poste-list.component';
+import { PosteDetailComponent } from './pages/postes/poste-detail/poste-detail.component';
 import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
@@ -10,5 +12,7 @@ export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] },
   { path: 'employes', component: EmployeListComponent, canActivate: [authGuard] },
-  { path: 'employes/:id', component: EmployeDetailComponent, canActivate: [authGuard] }
+  { path: 'employes/:id', component: EmployeDetailComponent, canActivate: [authGuard] },
+  { path: 'postes', component: PosteListComponent, canActivate: [authGuard] },
+  { path: 'postes/:id', component: PosteDetailComponent, canActivate: [authGuard] }
 ];
