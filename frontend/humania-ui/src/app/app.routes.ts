914453cccbb80e723bed7977121fd711ae87fe7f
Antonio2772 <atn.randr@gmail.com>
2025-11-08 13:17:57 +0300

    chore: add new link for the new pages

--- frontend/humania-ui/src/app/app.routes.ts
+++ frontend/humania-ui/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { EmployeListComponent } from './pages/employes/employe-list/employe-list.component';
+import { EmployeDetailComponent } from './pages/employes/employe-detail/employe-detail.component';
 import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] },
-  { path: 'employes', component: EmployeListComponent, canActivate: [authGuard] }
+  { path: 'employes', component: EmployeListComponent, canActivate: [authGuard] },
+  { path: 'employes/:id', component: EmployeDetailComponent, canActivate: [authGuard] }
 ];
